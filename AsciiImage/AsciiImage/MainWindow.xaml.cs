cf6c5360c34611859db059793e941af89cbd6e13
SpeedLight1221 <vojtacharouz@gmail.com>
2023-01-23 22:33:38 +0100

    Choose an Img and checks if its not too big

--- AsciiImage/AsciiImage/MainWindow.xaml.cs
+++ AsciiImage/AsciiImage/MainWindow.xaml.cs
@@ -13,19 +13,59 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using Microsoft.Win32;
+
+
 
 namespace AsciiImage
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    
     public partial class MainWindow : Window
     {
+        BitmapImage? img;
+        
         public MainWindow()
         {
+           
             InitializeComponent();
 
-            string x = "xd";
+            
         }
+
+        private void file_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Image files (*.png,*.jpeg,*.jpg)|*.jpeg;*.png";
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if(ofd.ShowDialog() == true)
+            {
+
+
+                img = new BitmapImage(new Uri(ofd.FileName));
+                if ((img.Width <= 800) && (img.Height <= 400))
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Obrázek je příliš velký");
+                }
+                
+
+                image.Source = img;
+            }
+           
+
+            
+        }
+        
+
+
+       
     }
 }
